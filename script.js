fca097f240d1a3f6938650ec73488dcfa51c3081
Marta <user@Marta-MacBook-Pro.local>
2022-07-21 16:32:22 +0300

    edit js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ for (let i = 0; i < element.length; i++) {
     nodes.getElementsByClassName("card")[i].onmousemove = function myMove() {
         element[i].style.transform = "rotateY(360deg)";
         element[i].style.transition = "5s";
-        element[i].style.transitionDelay = "1s";
+        element[i].style.transitionDelay = "0.5s";
     }
 }
